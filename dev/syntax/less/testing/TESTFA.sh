83dd116eebc0b7c89f71a4218ef9182b02cb81ab
ababaian <ababaian@bccrc.ca>
2017-12-01 08:54:52 -0800

    Updated less install scripts

--- dev/syntax/less/testing/TESTFA.sh
+++ dev/syntax/less/testing/TESTFA.sh
@@ -1,3 +1,3 @@
 source-highlight -f esc --lang-def=$PWD/fasta.lang \
  --outlang-def=$PWD/bioSyntax.outlang \
- --style-file=$PWD/fasta.style -i test_gcContent.fa
+ --style-file=$PWD/fasta.style -i data/test_gcContent.fa
---
+++ dev/syntax/less/testing/TESTPDB.sh
@@ -0,0 +1,3 @@
+source-highlight -f esc --lang-def=$PWD/pdb.lang \
+ --outlang-def=$PWD/bioSyntax-vcf.outlang \
+ --style-file=$PWD/pdb.style -i data/test.pdb
