e3100185cb0e51a496b3b284f376489ab99c5231
ababaian <ababaian@bccrc.ca>
2017-12-03 13:50:26 -0800

    Optimized less-VCF to work with large files

--- dev/syntax/less/testing/TESTVCF.sh
+++ dev/syntax/less/testing/TESTVCF.sh
@@ -1,3 +1,7 @@
 source-highlight -f esc --lang-def=$PWD/vcf.lang \
  --outlang-def=$PWD/bioSyntax-vcf.outlang \
- --style-file=$PWD/bioSyntax-vcf.style -i test_2.vcf
+ --style-file=$PWD/vcf.style -i data/test_5.vcf
+
+#source-highlight -f esc --lang-def=$PWD/vcf.lang \
+# --outlang-def=$PWD/bioSyntax-vcf.outlang \
+# --style-file=$PWD/vcf.style -i data/test_2.vcf
---
+++ dev/syntax/less/testing/TIMEVCF.sh
@@ -0,0 +1,15 @@
+source-highlight --statistics -f esc --lang-def=$PWD/vcf.lang \
+ --outlang-def=$PWD/bioSyntax-vcf.outlang \
+ --style-file=$PWD/vcf.style -i data/test_4.vcf | tail -n 1 -
+
+ source-highlight --statistics -f esc --lang-def=$PWD/vcf.lang \
+ --outlang-def=$PWD/bioSyntax-vcf.outlang \
+ --style-file=$PWD/vcf.style -i data/test_4.vcf | tail -n 1 -
+
+ source-highlight --statistics -f esc --lang-def=$PWD/vcf.lang \
+ --outlang-def=$PWD/bioSyntax-vcf.outlang \
+ --style-file=$PWD/vcf.style -i data/test_4.vcf | tail -n 1 -
+
+#source-highlight -f esc --lang-def=$PWD/vcf.lang \
+# --outlang-def=$PWD/bioSyntax-vcf.outlang \
+# --style-file=$PWD/vcf.style -i data/test_2.vcf
