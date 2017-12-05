49777d23c2bb94794c87c0bcb986d927c78fea37
ababaian <ababaian@bccrc.ca>
2017-12-05 10:50:06 -0800

    Sync less files. Bugfix in less-fq

--- dev/syntax/less/testing/TESTFQ.sh
+++ dev/syntax/less/testing/TESTFQ.sh
@@ -1,4 +1,4 @@
 gzip -dc test2.fq.gz |
 source-highlight -f esc --lang-def=$PWD/fastq.lang \
  --outlang-def=$PWD/bioSyntax.outlang \
- --style-file=$PWD/fasta.style -i "$source"
+ --style-file=$PWD/fasta.style -i data/test1.fq
