cd70e9feac9b20a2cdb10353239bee02dd76ecfe
ababaian <ababaian@bccrc.ca>
2017-12-08 10:37:09 -0800

    Started gedit theme unification for bed/clustal/faidx

--- syntax/gedit/quickInstall_unix.sh
+++ syntax/gedit/quickInstall_unix.sh
@@ -6,10 +6,10 @@ chmod 0644 *.lang
 
 sudo cp *.lang /usr/share/gtksourceview-3.0/language-specs/
 
-sudo cp bioKate.xml /usr/share/gtksourceview-3.0/styles/bioKate.xml
+sudo cp bioSyntax.xml /usr/share/gtksourceview-3.0/styles/bioSyntax.xml
 
 
 ## In Gedit you now need to select the bioKate Theme
 
 echo 'In Gedit select the bioKate Theme'
-echo 'Edit > Preferences > Font & Color > bioKate'
+echo 'Edit > Preferences > Font & Color > bioSyntax'
